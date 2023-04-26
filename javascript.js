0c5a59672442c10fb3b252832f07841807ae0f63
ja259 <127046977+ja259@users.noreply.github.com>
2023-04-25 22:03:29 -0400

    Add javascript.js

--- javascript.js
+++ javascript.js
@@ -1,2 +1,49 @@
 /* javascript.js */
+  
+const getPlayerChoice = playerInput =>{
+    playerInput = playerInput.toLowercase();
+    if(playerInput === 'rock' || playerInput === 'paper' || playerInput === 'scissors'){
+        return playerInput;
+    } else{
+        console.log('Error')
+    }
+}
 
+const getComputerChoice = () => {
+    let randomNumber = Math.floor(Math.random()*3);
+    switch(randomNumber){
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';        
+    }
+}
+
+const determineWinner = (playerSelection,computerSelection) => {
+    if( playerSelection === computerSelection){
+        return 'The game was a tie!';
+    }
+    if(playerSelection === 'rock'){
+        if(computerSelection === 'paper'){
+            return 'You Lose! Paper beats Rock';
+        }else{
+            return 'Player won!';
+        }
+    }
+    if(playerSelection === 'paper'){
+        if(computerSelection === 'scissors' || 'rock'){
+            return 'You lose! Scissors or rock beats paper';
+        }else{
+            return 'Player won!';
+        }
+    }
+    if(playerSelection === 'scissors'){
+        if(computerSelection === 'rock' || 'paper'){
+            return 'You lose! Rock or paper beats scissors';
+        }else{
+            return 'Player won!';
+        }
+    }
+}
