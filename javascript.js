a3616444999c16b2021ba5e801dcacc5b6ff8ee9
James Otwane <127046977+ja259@users.noreply.github.com>
2023-05-21 11:33:43 -0400

    Add index.html

--- javascript.js
+++ javascript.js
@@ -1,5 +1,46 @@
 /* javascript.js */
 
+
+const pScore = document.getElementById('playerScore');
+const cScore = document.getElementById('computerScore');
+const buttons = document.querySelectorAll('.selection button');
+const showIcon = document.querySelector('.show i');
+const computerShowIcon = document.querySelector('.computer i');
+
+let computerScore = 1;
+let playerScore = 1;
+
+const rockIcon = "fas fa-hand-rock";
+const paperIcon = "fas fa-hand-paper";
+const scissorsIcon = "fas fa-hand-scissors";
+
+const randomClasses = [rockIcon, paperIcon, scissorsIcon];
+const text = document.getElementById('demo');
+const text2 = document.getElementById('demo2');
+
+const tie = ()=>{
+    text.innerHTML = "It's a Tie! ";
+    text.style.color = 'orange';
+    text2.innerHTML = text.innerHTML;
+    text2.style.color = 'orange';
+}
+
+const win = ()=>{
+    text.innerHTML = "It's a Win! ";
+    text.style.color = 'rgb(1, 146, 1)';
+    text2.innerHTML = text.innerHTML;
+    text2.style.color = 'rgb(1, 146, 1)';
+}
+
+const lose = ()=>{
+    text.innerHTML = "You Lose! ";
+    text.style.color = 'red';
+    text2.innerHTML = text.innerHTML;
+    text2.style.color = 'red';
+}
+
+
+
 const getPlayerChoice = playerInput => {
     playerInput = playerInput.toLowerCase();
     if(playerInput === 'rock' || playerInput === 'paper' || playerInput === 'scissors'){
@@ -23,27 +64,27 @@ const getComputerChoice = () => {
 
 const determineWinner = (playerSelection,computerSelection) => {
     if( playerSelection === computerSelection){
-        return 'The game was a tie!';
+        return "It's a tie!";
     }
     if(playerSelection === 'rock'){
         if(computerSelection === 'paper'){
-            return 'You Lose! Paper beats Rock';
+            return 'You Lose!';
         }else{
-            return 'Player won!';
+            return " It's a win!";
         }
     }
     if(playerSelection === 'paper'){
         if(computerSelection === 'scissors' || 'rock'){
-            return 'You lose! Scissors or rock beats paper';
+            return 'You lose!';
         }else{
-            return 'Player won!';
+            return " It's a win!";
         }
     }
     if(playerSelection === 'scissors'){
         if(computerSelection === 'rock' || 'paper'){
-            return 'You lose! Rock or paper beats scissors';
+            return 'You lose!';
         }else{
-            return 'Player won!';
+            return " It's a win!";
         }
     }
 }
