b36f83cdf018a6a704e4f92593e4e60302224283
James Otwane <127046977+ja259@users.noreply.github.com>
2023-05-21 12:21:29 -0400

    Add index.html

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 /* javascript.js */
 
-
 const pScore = document.getElementById('playerScore');
 const cScore = document.getElementById('computerScore');
 const buttons = document.querySelectorAll('.selection button');
@@ -41,45 +40,45 @@ const lose = ()=>{
 
 const game = () =>{
     buttons.forEach(btn =>{
-        btn.addEventListener('click',(e)=>{
-        // Random rock paper scissor for the computer and clicked ones for the player
-           let clickedBtn = e.target.className;
-           showIcon.className = clickedBtn;
-           let randomNum = Math.floor(Math.random() * randomClasses.length);
-           computerShowIcon.className = randomClasses[randomNum];
+    btn.addEventListener('click',(e)=>{
+    // Random rock paper scissor for the computer and clicked ones for the player
+    let clickedBtn = e.target.className;
+    showIcon.className = clickedBtn;
+    let randomNum = Math.floor(Math.random() * randomClasses.length);
+    computerShowIcon.className = randomClasses[randomNum];
 
-           // If it's a Tie .
-           if(showIcon.className === computerShowIcon.className){
-               pScore.innerHTML = pScore.innerHTML;
-               cScore.innerHTML = cScore.innerHTML;
-               tie();
-           }
+    // If it's a Tie .
+if(showIcon.className === computerShowIcon.className){
+    pScore.innerHTML = pScore.innerHTML;
+    cScore.innerHTML = cScore.innerHTML;
+    tie();
+    }
 
-           // if it's not a Tie.
-           else if(showIcon.className === rockIcon && computerShowIcon.className === scissorsIcon){
-               pScore.innerHTML = playerScore;
-               playerScore++;
-               win();
-           }else if(showIcon.className === rockIcon && computerShowIcon.className === paperIcon){
-               cScore.innerHTML = computerScore;
-               computerScore++;
-               lose();
-           }else if(showIcon.className === paperIcon && computerShowIcon.className === scissorsIcon){
-               cScore.innerHTML = computerScore;
-               computerScore++;
-               lose();
-           }else if(showIcon.className === paperIcon && computerShowIcon.className === rockIcon){
-               pScore.innerHTML = playerScore;
-               playerScore++;
-               win();
-           }else if(showIcon.className === scissorsIcon && computerShowIcon.className === rockIcon){
-               cScore.innerHTML = computerScore;
-               computerScore++;
-               lose();
-           }else if(showIcon.className === scissorsIcon && computerShowIcon.className === paperIcon){
-               pScore.innerHTML = playerScore;
-               playerScore++;
-               win();
+    // if it's not a Tie.
+    else if(showIcon.className === rockIcon && computerShowIcon.className === scissorsIcon){
+       pScore.innerHTML = playerScore;
+       playerScore++;
+       win();
+    }else if(showIcon.className === rockIcon && computerShowIcon.className === paperIcon){
+        cScore.innerHTML = computerScore;
+        computerScore++;
+        lose();
+    }else if(showIcon.className === paperIcon && computerShowIcon.className === scissorsIcon){
+        cScore.innerHTML = computerScore;
+        computerScore++;
+        lose();
+    }else if(showIcon.className === paperIcon && computerShowIcon.className === rockIcon){
+        pScore.innerHTML = playerScore;
+        playerScore++;
+        win();
+    }else if(showIcon.className === scissorsIcon && computerShowIcon.className === rockIcon){
+        cScore.innerHTML = computerScore;
+        computerScore++;
+        lose();
+    }else if(showIcon.className === scissorsIcon && computerShowIcon.className === paperIcon){
+        pScore.innerHTML = playerScore;
+        playerScore++;
+        win();
            }
         });
     });
