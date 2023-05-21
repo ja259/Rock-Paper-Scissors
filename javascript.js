b2425b54331281c58a0e631e0bf6768213b8f501
James Otwane <127046977+ja259@users.noreply.github.com>
2023-05-21 11:51:16 -0400

    Add javascript.js

--- javascript.js
+++ javascript.js
@@ -39,57 +39,53 @@ const lose = ()=>{
     text2.style.color = 'red';
 }
 
+const game = () =>{
+    buttons.forEach(btn =>{
+        btn.addEventListener('click',(e)=>{
+        // Random rock paper scissor for the computer and clicked ones for the player
+           let clickedBtn = e.target.className;
+           showIcon.className = clickedBtn;
+           let randomNum = Math.floor(Math.random() * randomClasses.length);
+           computerShowIcon.className = randomClasses[randomNum];
 
+           // If it's a Tie .
+           if(showIcon.className === computerShowIcon.className){
+               pScore.innerHTML = pScore.innerHTML;
+               cScore.innerHTML = cScore.innerHTML;
+               tie();
+           }
 
-const getPlayerChoice = playerInput => {
-    playerInput = playerInput.toLowerCase();
-    if(playerInput === 'rock' || playerInput === 'paper' || playerInput === 'scissors'){
-        return playerInput;
-    } else{
-        console.log('Error!');
-    }
+           // if it's not a Tie.
+           else if(showIcon.className === rockIcon && computerShowIcon.className === scissorsIcon){
+               pScore.innerHTML = playerScore;
+               playerScore++;
+               win();
+           }else if(showIcon.className === rockIcon && computerShowIcon.className === paperIcon){
+               cScore.innerHTML = computerScore;
+               computerScore++;
+               lose();
+           }else if(showIcon.className === paperIcon && computerShowIcon.className === scissorsIcon){
+               cScore.innerHTML = computerScore;
+               computerScore++;
+               lose();
+           }else if(showIcon.className === paperIcon && computerShowIcon.className === rockIcon){
+               pScore.innerHTML = playerScore;
+               playerScore++;
+               win();
+           }else if(showIcon.className === scissorsIcon && computerShowIcon.className === rockIcon){
+               cScore.innerHTML = computerScore;
+               computerScore++;
+               lose();
+           }else if(showIcon.className === scissorsIcon && computerShowIcon.className === paperIcon){
+               pScore.innerHTML = playerScore;
+               playerScore++;
+               win();
+           }
+        });
+    });
 }
 
-const getComputerChoice = () => {
-    let randomNumber = Math.floor(Math.random()*3);
-    switch(randomNumber){
-        case 0:
-            return 'rock';
-        case 1:
-            return 'paper';
-        case 3:
-            return 'scissors';        
-    }
-}
+// Run the game.
+game();
 
-const determineWinner = (playerSelection,computerSelection) => {
-    if( playerSelection === computerSelection){
-        return "It's a tie!";
-    }
-    if(playerSelection === 'rock'){
-        if(computerSelection === 'paper'){
-            return 'You Lose!';
-        }else{
-            return " It's a win!";
-        }
-    }
-    if(playerSelection === 'paper'){
-        if(computerSelection === 'scissors' || 'rock'){
-            return 'You lose!';
-        }else{
-            return " It's a win!";
-        }
-    }
-    if(playerSelection === 'scissors'){
-        if(computerSelection === 'rock' || 'paper'){
-            return 'You lose!';
-        }else{
-            return " It's a win!";
-        }
-    }
-}
-function playRound(){
-    let playerSelection = getPlayerChoice('rock');
-    let computerSelection = getComputerChoice('scissors');
-   
-}
+
